fc79a07edbac12fc863a3d5eebd7df60dd357fe0
Boaz Shmueli <34333928+bshmueli@users.noreply.github.com>
2019-05-19 12:16:20 +0800

    add account controller

--- app/controllers/accounts.rb
+++ app/controllers/accounts.rb
@@ -27,11 +27,11 @@ module Vitae
 
         response.status = 201
         response['Location'] = "#{@account_route}/#{new_account.username}"
-        { message: 'Project saved', data: new_account }.to_json
+        { message: 'Account saved', data: new_account }.to_json
       rescue Sequel::MassAssignmentRestriction
         routing.halt 400, { message: 'Illegal Request' }.to_json
       rescue StandardError => e
-        puts error.inspect
+        puts e.inspect
         routing.halt 500, { message: e.message }.to_json
       end
     end
